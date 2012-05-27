65a038946ea646398dd546e26e9c68cfb38b5b2d
biancini <andrea.biancini@gmail.com>
2012-05-27 16:24:25 +0200

    Added NSS module.

--- libnss_shib.c
+++ libnss_shib.c
@@ -20,8 +20,8 @@
 BODY *body = NULL;
 BODY *body_pwd = NULL;
 BODY *body_grp = NULL;
-static int last_rownum_pwd = -1;
-static int last_rownum_grp = -1;
+int last_rownum_pwd = -1;
+int last_rownum_grp = -1;
 
 static const char *config_file = "/etc/libnss.conf";
 
@@ -30,6 +30,22 @@ const char *cafile = NULL;
 const char *sslcheck = NULL;
 const char *username = NULL;
 const char *password = NULL;
+
+#ifdef DEBUG
+void print_body(BODY *pointer)
+{
+  if (pointer == NULL) return;
+
+  fprintf(stderr, "-> Printing: ");
+  BODY *cursor = pointer;
+  while(cursor)
+  {
+    fprintf(stderr, "%s, ", cursor->row);
+    cursor = cursor->next;
+  }
+  fprintf(stderr, "\n");
+}
+#endif
 
 static void readconfig()
 {
@@ -111,7 +127,6 @@ size_t bodycallback(char *ptr, size_t size, size_t nmemb, void *userdata)
 char *getelementfrompasswd(int filter, const char *value, int column)
 {
   readconfig();
-fprintf(stderr, "---------> %s\n", url);
   char *newurl = (char *) malloc(sizeof(char)*(strlen(url)+8));
   sprintf(newurl, "%s?passwd", url);
   if (!geturl(newurl, username, password, cafile, sslcheck)) return NULL;
@@ -149,7 +164,7 @@ fprintf(stderr, "---------> %s\n", url);
 enum nss_status _nss_shib_getpwnam_r(const char *name, struct passwd *result, char *buffer, size_t buflen, int *errnop)
 {
   #ifdef DEBUG
-  fprintf(stderr, "Entering _nss_shib_getpwnam_r with name=%s.\n", name);
+  fprintf(stderr, "\nEntering _nss_shib_getpwnam_r with name=%s.\n", name);
   #endif
 
   readconfig();
@@ -192,7 +207,7 @@ enum nss_status _nss_shib_getpwnam_r(const char *name, struct passwd *result, ch
 enum nss_status _nss_shib_getpwuid_r(uid_t uid, struct passwd *result, char *buffer, size_t buflen, int *errnop)
 {
   #ifdef DEBUG
-  fprintf(stderr, "Entering _nss_shib_getpwuid_r with uid=%d.\n", uid);
+  fprintf(stderr, "\nEntering _nss_shib_getpwuid_r with uid=%d.\n", uid);
   #endif
 
   readconfig();
@@ -235,17 +250,17 @@ enum nss_status _nss_shib_getpwuid_r(uid_t uid, struct passwd *result, char *buf
 enum nss_status _nss_shib_setpwent(void)
 {
   #ifdef DEBUG
-  fprintf(stderr, "Entering _nss_shib_setpwent.\n");
+  fprintf(stderr, "\nEntering _nss_shib_setpwent.\n");
   #endif
 
-  syslog(LOG_ERR, "Unable to set passwd entities on Shibboleth.\n");
-  return NSS_STATUS_UNAVAIL;
+  last_rownum_pwd = -1;
+  return NSS_STATUS_SUCCESS;
 }
 
 enum nss_status _nss_shib_getpwent_r(struct passwd *result, char *buffer, size_t buflen, int *errnop)
 {
   #ifdef DEBUG
-  fprintf(stderr, "Entering _nss_shib_getpwent_r.\n");
+  fprintf(stderr, "\nEntering _nss_shib_getpwent_r (iter=%d).\n", last_rownum_pwd);
   #endif
 
   readconfig();
@@ -255,11 +270,11 @@ enum nss_status _nss_shib_getpwent_r(struct passwd *result, char *buffer, size_t
   body_pwd = body; body = NULL;
   if (body_pwd == NULL) return NSS_STATUS_UNAVAIL;
 
-  int i = -1;
+  int i = 0;
   BODY *cursor = body_pwd;
   while (cursor)
   {
-    if (i == last_rownum_pwd)
+    if (last_rownum_pwd == -1 && i > last_rownum_pwd)
     {
       char **array = split_str(cursor->row, ":");
       if (array != NULL)
@@ -276,8 +291,8 @@ enum nss_status _nss_shib_getpwent_r(struct passwd *result, char *buffer, size_t
         fprintf(stderr, "Found item: [name=%s, uid=%d, gid=%d]\n", array[0], atoi(array[2]), atoi(array[3]));
         #endif
 
-        last_rownum_pwd++;
-        return NSS_STATUS_SUCCESS;
+        last_rownum_pwd = i;
+        return NSS_STATUS_TRYAGAIN;
       }
     }
 
@@ -291,27 +306,27 @@ enum nss_status _nss_shib_getpwent_r(struct passwd *result, char *buffer, size_t
 enum nss_status _nss_shib_setgrent_r(struct group *result, void *args)
 {
   #ifdef DEBUG
-  fprintf(stderr, "Entering _nss_shib_setgrent_r.\n");
+  fprintf(stderr, "\nEntering _nss_shib_setgrent_r.\n");
   #endif
 
-  syslog(LOG_ERR, "Unable to set group entities on Shibboleth.\n");
-  return NSS_STATUS_UNAVAIL;
+  last_rownum_grp = -1;
+  return NSS_STATUS_SUCCESS;
 }
 
 enum nss_status _nss_shib_endgrent_r(struct group *result, void* args)
 {
   #ifdef DEBUG
-  fprintf(stderr, "Entering _nss_shib_endgrent_r.\n");
+  fprintf(stderr, "\nEntering _nss_shib_endgrent_r.\n");
   #endif
 
-  syslog(LOG_ERR, "Unable to end group entities on Shibboleth.\n");
-  return NSS_STATUS_UNAVAIL;
+  last_rownum_grp = -1;
+  return NSS_STATUS_SUCCESS;
 }
 
 enum nss_status _nss_shib_getgrent_r(struct group *result, char *buffer, size_t buflen, int *errnop)
 {
   #ifdef DEBUG
-  fprintf(stderr, "Entering _nss_shib_getgrent_r.\n");
+  fprintf(stderr, "\nEntering _nss_shib_getgrent_r (iter=%d).\n", last_rownum_grp);
   #endif
 
   readconfig();
@@ -321,11 +336,11 @@ enum nss_status _nss_shib_getgrent_r(struct group *result, char *buffer, size_t 
   body_grp = body; body = NULL;
   if (body_grp == NULL) return NSS_STATUS_UNAVAIL;
 
-  int i = -1;
+  int i = 0;
   BODY *cursor = body_grp;
   while (cursor)
   {
-    if (i == last_rownum_grp)
+    if (last_rownum_grp == -1 || i > last_rownum_grp)
     {
       char **array = split_str(cursor->row, ":");
       if (array != NULL)
@@ -339,22 +354,22 @@ enum nss_status _nss_shib_getgrent_r(struct group *result, char *buffer, size_t 
         fprintf(stderr, "Found item: [grname=%s, gid=%d]\n", array[0], atoi(array[2]));
         #endif
 
-        last_rownum_grp++;
-        return NSS_STATUS_SUCCESS;
+        last_rownum_grp = i;
+        return NSS_STATUS_TRYAGAIN;
       }
     }
 
     i++;
     cursor = cursor->next;
   }  
-
+  fprintf(stderr, "non dovresti esser qui.\n");
   return NSS_STATUS_NOTFOUND;
 }
 
 enum nss_status _nss_shib_getgrnam_r(const char *name, struct group *result, char *buffer, size_t buflen, int *errnop)
 {
   #ifdef DEBUG
-  fprintf(stderr, "Entering _nss_shib_getgrnam_r with grname=%s.\n", name);
+  fprintf(stderr, "\nEntering _nss_shib_getgrnam_r with grname=%s.\n", name);
   #endif
 
   readconfig();
@@ -394,7 +409,7 @@ enum nss_status _nss_shib_getgrnam_r(const char *name, struct group *result, cha
 enum nss_status _nss_shib_getgrgid_r(gid_t gid, struct group *result, char *buffer, size_t buflen, int *errnop)
 {
   #ifdef DEBUG
-  fprintf(stderr, "Entering _nss_shib_getgrnam_r with gid=%d.\n", gid);
+  fprintf(stderr, "\nEntering _nss_shib_getgrnam_r with gid=%d.\n", gid);
   #endif
 
   readconfig();
@@ -430,75 +445,6 @@ enum nss_status _nss_shib_getgrgid_r(gid_t gid, struct group *result, char *buff
 
   return NSS_STATUS_NOTFOUND;
 }
-
-enum nss_status _nss_shib_getgroupsbymember_r(const char *member, struct group *result, char *buffer, size_t buflen, int *errnop)
-{
-  #ifdef DEBUG
-  fprintf(stderr, "Entering _nss_shib_getgroupsbymember_r with member=%s.\n", member);
-  #endif
-
-  readconfig();
-  char *newurl = (char *) malloc(sizeof(char)*(strlen(url)+7));
-  sprintf(newurl, "%s?group", url);
-  if (!geturl(newurl, username, password, cafile, sslcheck)) return NSS_STATUS_UNAVAIL;
-  body_grp = body; body = NULL;
-  if (body_grp == NULL) return NSS_STATUS_UNAVAIL;
-
-  BODY *cursor = body_grp;
-  while (cursor)
-  {
-    char **array = split_str(cursor->row, ":");
-    if (array != NULL)
-    {
-      if (strcmp(array[0], member) == 0)
-      {
-        result->gr_name = array[0];
-        result->gr_passwd = array[1];
-        result->gr_gid = atoi(array[2]);
-        result->gr_mem = split_str(array[3], ",");
-
-        #ifdef DEBUG
-        fprintf(stderr, "Found item for member=%s: [gid=%d]\n", member, atoi(array[2]));
-        #endif
-
-        return NSS_STATUS_SUCCESS;
-      }
-    }
-
-    cursor = cursor->next;
-  }  
-
-  return NSS_STATUS_NOTFOUND;
-}
-
-/*
-static enum nss_status _nss_shib_passwd_destr(nss_backend_t *pw_context, void *args)
-{
-  free(be);
-  syslog(LOG_DEBUG, "_nss_shib_passwd_destr");
-  return NSS_STATUS_SUCCESS;
-}
-
-static nss_backend_op_t passwd_ops[] = {
-  _nss_shib_passwd_destr,
-  _nss_shib_setpwent_r,         // NSS_DBOP_SETENT
-  _nss_shib_getpwent_r,         // NSS_DBOP_GETENT
-  _nss_shib_getpwnam_r,         // NSS_DBOP_PASSWD_BYNAME
-  _nss_shib_getpwuid_r          // NSS_DBOP_PASSWD_BYUID
-};
-
-nss_backend_t *_nss_shib_passwd_constr(const char *db_name, const char *src_name, const char *cfg_args)
-{
-  nss_backend_t *be;
-
-  if(!(be = (nss_backend_t *)malloc(sizeof(nss_backend_t)))) return NULL;
-  be->ops = passwd_ops;
-  be->n_ops = sizeof(passwd_ops) / sizeof(nss_backend_op_t);
-
-  syslog(LOG_DEBUG, "Initialized nss_shib passwd backend");
-  return be;
-}
-*/
 
 #ifdef DEBUG
 int main(int argc, char *argv[])
