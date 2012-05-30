6e928a13bbc98a9081492c531a25d3a15ad1c2f0
biancini <andrea.biancini@gmail.com>
2012-05-30 11:57:07 +0200

    Added some more comment on configuration files

--- httpd/pam.php
+++ httpd/pam.php
@@ -17,27 +17,41 @@ via Shibboleth after the user authentication and session creation.
 
 ***********************************************************************************************/
 
+// At first put a value in session called autheticated, this value will contain true or false
+// depending on if the user should be granted access or not.
+print "authenticated=".(eval_authenticateduser() ? "true" : "false")."\n";
+
+// Then all the specified attributes in the $_SERVER variable are put into user session.
+// The $headers array contains a list of variable names to be added to the session
 $headers = array('Shib-Application-ID', 'Shib-Session-ID', 'Shib-Identity-Provider', 'Shib-Authentication-Instant',
                  'Shib-Authentication-Method', 'Shib-AuthnContext-Class', 'Shib-Session-Index',
                  'eduPersonEntitlement', 'eduPersonPrincipalName', 'eduPersonScopedAffiliation',  'eduPersonTargetedID',
                  'email', 'givenName', 'mail', 'name', 'sn', 'surname', 'uid');
 
-print "authenticated=true\n";
 
 foreach($_SERVER as $key => $value) {
-	if(in_array($key, $headers)) {
-		print $key."=".$value."\n";
-	}
+        if(in_array($key, $headers)) {
+                print $key."=".$value."\n";
+        }
 }
 
+// At last the unique key of the cookie whose name starts with "_shibsession_" is added to the user session.
+// This key is necessary to retrieve the session from the SP in further HTTP calls.
 $cookies = $_SERVER["HTTP_COOKIE"];
 foreach (explode(";", $cookies) as $curcookie) {
         if (strpos($curcookie, "=") !== false) {
                 list($cookiename, $cookieval) = explode("=", trim($curcookie));
                 if (strpos($cookiename, "_shibsession_") !== false) {
                         print "eccisiamo=si\n";
-                        print "Shib_Session_Unique=".trim(str_replace("_shibsession_", "", $cookiename));
+                        print "Shib_Session_Unique=".trim(str_replace("_shibsession_", "", $cookiename)."\n");
                 }
         }
 }
+
+// Function implementing the logic of user authentication.
+// Returns true if the user should be granted access and false otherwise.
+function eval_authenticateduser() {
+        return true;
+}
 ?>
+
