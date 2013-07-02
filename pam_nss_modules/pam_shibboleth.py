885a72442c580a39a5bd99b643b183a4c3143023
Andrea Biancini <andrea.biancini@gmail.com>
2013-07-02 16:04:57 +0200

    Added WAYF and Simple SAML support

--- pam_nss_modules/pam_shibboleth.py
+++ pam_nss_modules/pam_shibboleth.py
@@ -1,25 +1,30 @@
 session = {}
 
-def geturl(params, username, password):
-  import mechanize
-  import cookielib
-  import urllib
-  global session
+def is_shibbolethds(html_content):
+  return "wayf.css" in html_content
 
-  br = mechanize.Browser()
-  cj = cookielib.LWPCookieJar()
-  br.set_cookiejar(cj)
-  br.set_handle_equiv(True)
-  br.set_handle_gzip(False)
-  br.set_handle_redirect(True)
-  br.set_handle_referer(True)
-  br.set_handle_robots(False)
-  br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-  br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+def shibbolethds(pamh, br, params):
+  br.select_form(nr=0)
 
-  # Get first url and get redirected to IdP login page
-  br.open(params['url'])
+  num = 1
+  msg = "Chose the IdP you want to use for login. Available IdPs are:\n"
+  listidps = {}
+  for c in br.form.find_control('origin').get_items():
+    msg = "%s%d. %s\n" % (msg, num, c.name)
+    listidps[num] = c.get_labels()[0].text
+    num = num+1
+  msg ="%s\nInsert the number of the IdP to be used: " % msg
 
+  rsp = send_msg(pamh, pamh.PAM_PROMPT_ECHO_ON, msg)
+  br.form['origin'] = [listidps[int(rsp.resp)]]
+  r = br.submit()
+  html_content = r.read()
+  return html_content
+
+def is_shibboleth(html_content):
+  return ('j_username' in html_content and 'j_password' in html_content)
+
+def shibboleth(pamh, br, params, username, password):
   # Submit first form with username and password provided
   br.select_form(nr=0)
   br.form['j_username'] = username
@@ -46,8 +51,67 @@ def geturl(params, username, password):
   # Submit form to create user session
   br.select_form(nr=0)
   r = br.submit()
+  html_content = r.read()
+  return html_content
+
+def is_simplesaml(html_content):
+  return ('id="username"' in html_content and 'id="password"' in html_content)
+
+def simplesaml(pamh, br, params, username, password):
+  # Submit first form with username and password provided
+  br.select_form(nr=0)
+  br.form['username'] = username
+  br.form['password'] = password
+  r = br.submit()
 
   html_content = r.read()
+
+  # Submit form to create user session
+  br.select_form(nr=0)
+  r = br.submit()
+  html_content = r.read()
+
+  #print html_content
+  return html_content
+
+def geturl(pamh, params, username, password):
+  import mechanize
+  import cookielib
+  import urllib
+  global session
+
+  br = mechanize.Browser()
+  cj = cookielib.LWPCookieJar()
+  br.set_cookiejar(cj)
+  br.set_handle_equiv(True)
+  br.set_handle_gzip(False)
+  br.set_handle_redirect(True)
+  br.set_handle_referer(True)
+  br.set_handle_robots(False)
+  br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+  br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+
+  # Get first url and get redirected to IdP login page
+  r = br.open(params['url'])
+  html_content = r.read()
+
+  continue_loop = True
+  while continue_loop:
+    if is_shibbolethds(html_content):
+      #print "It is a Shibboleth DS page"
+      html_content = shibbolethds(pamh, br, params)
+    elif is_shibboleth(html_content):
+      #print "It is a Shibboleth IdP page"
+      html_content = shibboleth(pamh, br, params, username, password)
+      continue_loop = False
+    elif is_simplesaml(html_content):
+      #print "It is a Simple SAML IdP page"
+      html_content = simplesaml(pamh, br, params, username, password)
+      continue_loop = False
+    else:
+      html_content = ""
+      continue_loop = False
+
   #print html_content
   session = {}
 
@@ -99,11 +163,11 @@ def pam_sm_authenticate(pamh, flags, argv):
     params = parse_args(pamh, argv)
     username = pamh.get_user(None)
     password = send_msg(pamh, pamh.PAM_PROMPT_ECHO_OFF, "%s's password:" % username)
-    geturl(params, username, password.resp)
+    geturl(pamh, params, username, password.resp)
   except pamh.exception, e:
     return e.pam_result
 
-  if params['sess_username'] in session: pamh.user = session[params['sess_username']]
+  if params['sess_username'] in session: pam.user = session[param['sess_username']]
   if 'authenticated' in session and session['authenticated'] == 'true':
     return pamh.PAM_SUCCESS
   else:
@@ -128,8 +192,8 @@ def pam_sm_chauthtok(pamh, flags, argv):
   return pamh.PAM_SYSTEM_ERR
 
 if __name__ == "__main__":
-    params = {'url': 'https://servername/secure/pam.php',
+    params = {'url': 'https://sp-servername/secure/pam.php',
               'sslcheck': False,
               'sess_username': 'uid'};
-    geturl(params, 'user', 'password')
-    print "Session values: %s" % session
+    geturl(None, params, 'username', 'password')
+    print session
